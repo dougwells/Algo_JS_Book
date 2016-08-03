f6a2d72028bf58eabc02d705ffd5a3c6359ada83
dougwells <dougwells@gmail.com>
2016-08-03 11:59:42 -0600

    Stacks and arrays

--- DW-practice/arrays.js
+++ DW-practice/arrays.js
@@ -3,14 +3,14 @@
 "use strict"
 let stack = [];
 
-//Add to stack
+//Add to front of array
 for(var i=1; i<=10; i++){
   stack.push(i);
 }
 console.log(stack);
 
-//Extract from end of stack
+//Extract from top of stack
 console.log("Take off end of stack", stack.pop(), stack);
 
-//Extract from start of array
+//Extract from start of array (bottom of stack)
 console.log("Take off start of array", stack.shift(), stack)
