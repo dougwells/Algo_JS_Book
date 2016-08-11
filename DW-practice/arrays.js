23e5365c50df048a79cc21c2659a14c8864755c5
dougwells <dougwells@gmail.com>
2016-08-10 18:53:52 -0600

    array method practice

--- DW-practice/arrays.js
+++ DW-practice/arrays.js
@@ -14,3 +14,12 @@ console.log("Take off end of stack", stack.pop(), stack);
 
 //Extract from start of array (bottom of stack)
 console.log("Take off start of array", stack.shift(), stack)
+
+//forEach merely iterates over each item.  No new array
+var forEachEx = stack.forEach(function(i){console.log(i+1);});
+console.log(".forEach -> Plus 1", forEachEx, "origin", stack);
+
+
+//.map() and .filter return NEW arrays
+var mapExArr = stack.map(function(item){return item+1;})
+console.log(".map -> Plus 1", mapExArr, "origin", stack);
