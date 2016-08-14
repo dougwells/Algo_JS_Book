85a7a9af4b1fd4a0f30d1eb3df7068d300072f1c
dougwells <dougwells@gmail.com>
2016-08-14 07:30:21 -0600

    Practice with Ques.  Circular Ques.

---
+++ DW-practice/circularQue.js
@@ -0,0 +1,56 @@
+function Queue() {
+
+    let items = [];
+
+    this.enqueue = function(element){
+        items.push(element);
+    };
+
+    this.dequeue = function(){
+        return items.shift();
+    };
+
+    this.front = function(){
+        return items[0];
+    };
+
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+
+    this.clear = function(){
+        items = [];
+    };
+
+    this.size = function(){
+        return items.length;
+    };
+
+    this.print = function(){
+        console.log(items.toString());
+    };
+}
+
+function hotPotato (nameList, num){
+
+    let queue = new Queue();
+
+    for (let i=0; i<nameList.length; i++){
+        queue.enqueue(nameList[i]);
+    }
+
+    let eliminated = '';
+    while (queue.size() > 1){
+        for (let i=0; i<num; i++){
+            queue.enqueue(queue.dequeue());
+        }
+        eliminated = queue.dequeue();
+        console.log(eliminated + ' was eliminated from the Hot Potato game.');
+    }
+
+    return queue.dequeue();
+}
+
+let names = ['John','Jack','Camila','Ingrid','Carl'];
+let winner = hotPotato(names, 7);
+console.log('The winner is: ' + winner);
