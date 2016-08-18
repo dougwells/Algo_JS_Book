7271430849674b8a2658630ebca9f7855fd2ca6f
dougwells <dougwells@gmail.com>
2016-08-18 08:01:52 -0600

    Algorithms: Binary Search Tree.  Constructing the tree

---
+++ DW-practice/binaryTree.js
@@ -0,0 +1,51 @@
+function BinarySearchTree() {
+
+    var Node = function(key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    };
+
+    var root = null;
+
+    this.insert = function(key){
+
+        var newNode = new Node(key);
+
+        //special case - first element
+        if (root === null){
+            root = newNode;
+        } else {
+            insertNode(root,newNode);
+        }
+    };
+
+    var insertNode = function(node, newNode){
+        if (newNode.key < node.key){
+            if (node.left === null){
+                node.left = newNode;
+            } else {
+                insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null){
+                node.right = newNode;
+            } else {
+                insertNode(node.right, newNode);
+            }
+        }
+    };
+
+    this.inOrderTraverse = function(callback){
+        inOrderTraverseNode(root, callback);
+    };
+
+    var inOrderTraverseNode = function (node, callback) {
+        if (node !== null) {
+            inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            inOrderTraverseNode(node.right, callback);
+        }
+    };
+
+  }
